82d2ce2c0dbbf0037e0f74eb1e60a79375f136b3
danielreedy21 <dreedy@luc.edu>
2023-02-21 20:36:20 -0600

    connected google maps api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,57 @@
 <script>
-	import '../app.css';
+	import Map from './Map.svelte'
 	let a = 2;
 	let b = 4;
 
 	function increaseA() {
 		a++;
 	}
+
 	function increaseB() {
 		b++;
 	}
+
 	function decreaseA() {
 		a--;
 	}
+
 	function decreaseB() {
 		b--;
 	}
 </script>
 
-<header class="flex justify-center">
+<header class="text-red-700 flex justify-center">
 	<p>{a} + {b} = {a + b}</p>
 </header>
+
 <div class="flex flex-row place-content-evenly font-bold">
-	<button class="" on:click={increaseA}> Increase A </button>
-	<button on:click={increaseB}> Increase B </button>
-	<button on:click={decreaseA}> Decrease A </button>
-	<button on:click={decreaseB}> Decrease B </button>
+
+	<button class="text-black
+	rounded-md border-4 border-black 
+	px-4 bg-sky-700 drop-shadow-2xl
+	hover:shadow-lg shadow-black" 
+	on:click={increaseA}> Increase A </button>
+
+	<button class="text-black
+	rounded-md border-4 border-black 
+	px-4 bg-sky-700 drop-shadow-2xl
+	hover:shadow-lg shadow-black" 
+	on:click={increaseB}> Increase B </button>
+
+	<button class="text-black
+	rounded-md border-4 border-black 
+	px-4 bg-sky-700 
+	hover:shadow-lg shadow-black" 
+	on:click={decreaseA}> Decrease A </button>
+
+	<button class="text-black
+	rounded-md border-4 border-black 
+	px-4 bg-sky-700 drop-shadow-2xl
+	hover:shadow-lg shadow-black" 
+	on:click={decreaseB}> Decrease B </button>
+
 </div>
 
-<!-- <Map /> -->
+<Map></Map>
 <style lang="postcss">
 </style>
--- src/routes/Map.svelte
+++ src/routes/Map.svelte
@@ -1,11 +1,26 @@
 <script>
-	// let container;
-	// let map;
-	// let zoom;
-	// let center = { lat: -34.397, lng: 150.644 };
+	import { onMount } from 'svelte';
+	import { Loader } from '@googlemaps/js-api-loader';
+
+	let map;
+
+	onMount(() => {
+		const loader = new Loader({
+			apiKey: 'AIzaSyC4cyPSREETc0xru4HcwURRDkIsmYlsHok',
+			version: 'weekly',
+			libraries: ['places']
+		});
+
+		loader.load().then(() => {
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: { lat: -34.397, lng: 150.644 },
+				zoom: 8
+			});
+		});
+	});
 </script>
 
-<p>Map goes here!</p>
+<p class="text-center">Map goes here!</p>
+<div id="map" class="h-screen w-full" />
 
 <style></style>
-
